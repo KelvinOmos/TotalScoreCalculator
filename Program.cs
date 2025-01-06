48827e9ea2620a4a1c0ebc7dd574e45acbddeffd
KelvinOmos <kelvinomos82@gmail.com>
2025-01-06 20:18:59 +0100

    Completed Task

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TotalScoreCalculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Example input array
+            int[] firstExample = {1,2,3,4,5};
+            int[] secondExample = {15, 25, 35};
+            int[] thirdExample = {8,8};
+
+            int firstTotalScore = ScoreCalculator.CalculateScore(firstExample);
+            int secondTotalScore = ScoreCalculator.CalculateScore(secondExample);
+            int thirdTotalScore = ScoreCalculator.CalculateScore(thirdExample);
+            Console.WriteLine($"The total score for the first input array ({string.Join(", ", firstExample)}) is: {firstTotalScore}");
+            Console.WriteLine($"The total score for the second input array ({string.Join(", ", secondExample)}) is: {secondTotalScore}");
+            Console.WriteLine($"The total score for the third input array ({string.Join(", ", thirdExample)}) is: {thirdTotalScore}");
+        }
+    }
+}
---
+++ ScoreCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalScoreCalculator
+{
+    public static class ScoreCalculator
+    {
+        public static int CalculateScore(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                Console.WriteLine("Enter one or more numbers");
+                return 0;
+            }
+            int score = 0;
+            foreach (var number in numbers)
+            {
+                if (number == 8)
+                {
+                    score += 5;
+                }
+                if (number % 2 == 0)
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score += 3;
+                }
+            }
+            return score;
+        }
+    }
+}
